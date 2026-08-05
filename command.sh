5a8df6595df0ac385bf7ab1af63d7db23697edb2
Thanaphon-673020253-2 <thanaphon.taw@kkumail.com>
2026-08-05 17:16:30 +0700

    merge main with dw_duckdb

---
+++ command.sh
@@ -0,0 +1,11 @@
+#install dpt-duckdb
+pip install dbt-duckdb
+
+# initialize dbt project
+pip intit <dbt-project-name>
+
+#jump into the dbt project directory
+cd <dbt-project-name>
+
+#check debug connection
+dbt debugstreamlit
